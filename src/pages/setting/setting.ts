b2b27b1f5f3387bdcaac81ddc84eab1ee53a7cbe
Aaron <aagavin@gmail.com>
2017-04-24 13:56:22 -0400

    added update function

--- src/pages/setting/setting.ts
+++ src/pages/setting/setting.ts
@@ -1,14 +1,26 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Http, HttpModule, ConnectionBackend } from "@angular/http";
+import { NavController, LoadingController, Loading } from 'ionic-angular';
+import { ApiProvider } from "../../providers/api-provider";
 
 @Component({
   selector: 'page-setting',
-  templateUrl: 'setting.html'
+  templateUrl: 'setting.html',
+  providers: [ApiProvider]
 })
 export class SettingPage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private apiProvider: ApiProvider, private http: Http, private loadingCtrl: LoadingController) { }
 
+  public updateRouteList(): void {
+    let loading: Loading = this.loadingCtrl.create({
+      content: 'Loading routes'
+    });
+    loading.present();
+    console.log('clicked update Route List');
+    this.apiProvider.updateStorage().then(value =>{
+      loading.dismiss();
+    });
   }
 
 }
