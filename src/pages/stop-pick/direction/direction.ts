ff702669b268ced45c07cdad2d9ab1e30751bb22
Aaron <aagavin@gmail.com>
2017-04-26 21:51:32 -0400

    more dir stuff

--- src/pages/stop-pick/direction/direction.ts
+++ src/pages/stop-pick/direction/direction.ts
@@ -1,24 +1,54 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { ViewController, NavParams } from 'ionic-angular';
 
-/**
- * Generated class for the Direction page.
- *
- * See http://ionicframework.com/docs/components/#navigation for more info
- * on Ionic pages and navigation.
- */
-@IonicPage()
+import { ApiProvider } from "../../../providers/api-provider";
+
 @Component({
   selector: 'page-direction',
   templateUrl: 'direction.html',
+  providers: [ApiProvider]
 })
 export class Direction {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  public directions: Array<Object>;
+  public directionsFiltered: Array<Object>;
+  public searchValue;
+
+  constructor(private viewController: ViewController, private apiProvider: ApiProvider, private navParams: NavParams) {
+    this.searchValue = '';
+
+    this.apiProvider.getDirection(this.navParams.get('route'))
+      .then(directions => {
+        this.directions = directions;
+        this.directionsFiltered = directions;
+      })
+      .catch(err => console.log(err));
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad Direction');
+  public dismiss(item?) {
+    if (typeof item === 'undefined') {
+      this.viewController.dismiss(null);
+    }
+    else {
+      this.viewController.dismiss(item);
+    }
+
   }
+
+  public onInput(event): void {
+    // console.log(this.searchValue);
+    this.directionsFiltered = this.directions.filter(value => {
+      if ((value['title'].toLocaleLowerCase()).includes(this.searchValue)) {
+        return value;
+      }
+    });
+  }
+
+  public onCancel(): void {
+    console.log('cancelled !');
+    this.directionsFiltered = this.directions;
+  }
+
+  public ionViewDidLoad() { console.log('ionViewDidLoad Route'); }
 
 }
