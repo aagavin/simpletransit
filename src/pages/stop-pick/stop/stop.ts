e5e0697c31aa49508c0217888d836e8ca3f29654
Aaron <aagavin@gmail.com>
2017-05-07 16:28:23 -0400

    stop page

--- src/pages/stop-pick/stop/stop.ts
+++ src/pages/stop-pick/stop/stop.ts
@@ -1,24 +1,80 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { ViewController, NavParams } from 'ionic-angular';
+import { ApiProvider } from "../../../providers/api-provider";
 
-/**
- * Generated class for the Stop page.
- *
- * See http://ionicframework.com/docs/components/#navigation for more info
- * on Ionic pages and navigation.
- */
-@IonicPage()
 @Component({
   selector: 'page-stop',
   templateUrl: 'stop.html',
+  providers: [ApiProvider]
 })
 export class Stop {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  private stops: object[];
+  private stopsFiltered: object[];
+  public searchValue;
+
+  /**
+   * Creates an instance of Stop.
+   * @param {ViewController} viewController 
+   * @param {NavParams} navParams 
+   * @param {ApiProvider} apiProvider 
+   * 
+   * @memberof Stop
+   */
+  constructor(private viewController: ViewController, private navParams: NavParams, private apiProvider: ApiProvider) {
+    this.searchValue = '';
+    let stop = this.navParams.get('stop');
+    let direction = this.navParams.get('direction');
+
+    this.apiProvider.getStopsByDirection(stop, direction)
+      .then(value => {
+        this.stops = value;
+        this.stopsFiltered = value
+      })
+      .catch(err => {
+        console.log(err);
+      })
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad Stop');
+  public ionViewDidLoad() { console.log('ionViewDidLoad Stop'); }
+
+  /**
+   * Closes the modal and passes data back
+   * 
+   * @param {any} [item] 
+   * 
+   * @memberof Stop
+   */
+  public dismiss(item?) {
+    if (typeof item === 'undefined') {
+      this.viewController.dismiss(null);
+    }
+    else {
+      this.viewController.dismiss(item);
+    }
   }
+
+  /**
+   * Filters the results based on the search query
+   * 
+   * @param {any} event 
+   * 
+   * @memberof Stop
+   */
+  public oninput(event): void {
+    this.stopsFiltered = this.stops.filter(value => {
+      if (value['title'].toLocaleLowerCase().includes(this.searchValue)) {
+        return value;
+      }
+    })
+  }
+
+  /**
+   * Clears the search key
+   * 
+   * 
+   * @memberof Stop
+   */
+  public onCancel(): void { this.stopsFiltered = this.stops; }
 
 }
