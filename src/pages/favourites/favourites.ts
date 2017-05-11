5f2d35d56ac030ef9f970ce4ce3a49903fe6c600
Aaron <cloud9ide@aagavin.ca>
2017-05-11 20:30:46 +0000

    add providers to favourites

--- src/pages/favourites/favourites.ts
+++ src/pages/favourites/favourites.ts
@@ -1,16 +1,19 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { ToastController } from 'ionic-angular';
+import { SMS } from '@ionic-native/sms';
+
+import { FavouriteProvider } from "../../providers/favourite/favourite";
 
 
 @Component({
   selector: 'favourites',
   templateUrl: 'favourites.html',
-  providers: []
+  providers: [SMS, FavouriteProvider]
 })
 export class FavouritesPage {
 
-  constructor(public navCtrl: NavController, private toastController:ToastController) {
+  constructor(private navCtrl: NavController, private toastController:ToastController, private sms:SMS) {
 
   }
 
