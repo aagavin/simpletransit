78fc984f56212942d27c3fccd01afa29df17a23d
Aaron <aagavin@gmail.com>
2017-05-11 21:15:56 -0400

    favourite getting

--- src/pages/favourites/favourites.ts
+++ src/pages/favourites/favourites.ts
@@ -3,7 +3,10 @@ import { NavController } from 'ionic-angular';
 import { ToastController } from 'ionic-angular';
 import { SMS } from '@ionic-native/sms';
 
+import { Http } from "@angular/http";
+
 import { FavouriteProvider } from "../../providers/favourite/favourite";
+import {Observable} from 'rxjs/Rx';
 
 
 @Component({
@@ -13,8 +16,43 @@ import { FavouriteProvider } from "../../providers/favourite/favourite";
 })
 export class FavouritesPage {
 
-  constructor(private navCtrl: NavController, private toastController:ToastController, private sms:SMS) {
+  private favArr: object[];
 
+  constructor(
+    private navCtrl: NavController,
+    private toastController: ToastController,
+    private sms: SMS,
+    private favouriteProvider: FavouriteProvider,
+    private http: Http
+  ) {
+
+  }
+
+  public ionViewDidLoad() {
+    console.log('loads favourite');
+
+    this.favouriteProvider.getAllFavourites()
+      .then(favourites => {
+        console.log(favourites);
+        this.getStopTimes(favourites);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+
+  }
+
+  public getStopTimes(favourites: object[]): void {
+    let favObj: Observable<object>[] = []
+
+    favourites.forEach(favourite => {
+      let url: string = `http://restbus.info/api/agencies/ttc/routes/${favourites['route']}/stops/${favourites['id']}/predictions`;
+      favObj.push(this.http.get(url).map(res => res.json()));
+    });
+
+    Observable.forkJoin(favObj).subscribe(value =>{
+      console.log(value);
+    })
   }
 
 }
