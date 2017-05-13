e9408506ee9680913a91bd6ca088f3e033a6a249
Aaron <aagavin@gmail.com>
2017-05-13 17:45:07 -0400

    remove favourites

--- src/pages/setting/setting.ts
+++ src/pages/setting/setting.ts
@@ -2,27 +2,48 @@ import { Component } from '@angular/core';
 import { Http } from "@angular/http";
 import { NavController, LoadingController, Loading } from 'ionic-angular';
 import { ApiProvider } from "../../providers/api-provider";
+import { FavouriteProvider } from "../../providers/favourite/favourite";
 
 @Component({
   selector: 'page-setting',
   templateUrl: 'setting.html',
-  providers: [ApiProvider]
+  providers: [ApiProvider, FavouriteProvider]
 })
 export class SettingPage {
 
-  constructor(public navCtrl: NavController, private apiProvider: ApiProvider, private http: Http, private loadingCtrl: LoadingController) { }
+  constructor(
+    public navCtrl: NavController,
+    private apiProvider: ApiProvider,
+    private http: Http,
+    private loadingCtrl: LoadingController,
+    private favouriteProvider: FavouriteProvider
+  ) { }
 
   public updateRouteList(): void {
     let loading: Loading = this.loadingCtrl.create({
       content: 'Loading routes'
     });
     loading.present();
-    
+
     console.log('clicked update Route List');
 
-    this.apiProvider.updateStorage().then(value =>{
+    this.apiProvider.updateStorage().then(value => {
       loading.dismiss();
     });
+  }
+
+
+  public removeAllFavourites(): void {
+    let loading: Loading = this.loadingCtrl.create({
+      content: 'Removing All Favourites'
+    });
+    loading.present();
+    this.favouriteProvider.removeAllFavourites()
+      .then(value => loading.dismiss())
+      .catch(err => {
+        console.log(err);
+        loading.dismiss();
+      });
   }
 
 }
