7a49310a44fcc00cfa1829a8c6ffb4d238009134
Aaron <aagavin@gmail.com>
2017-05-15 21:28:34 -0400

    comments fixed bug when removeing item

--- src/pages/favourites/favourites.ts
+++ src/pages/favourites/favourites.ts
@@ -16,29 +16,77 @@ import { Observable } from 'rxjs/Rx';
 })
 export class FavouritesPage {
 
+  /**
+   * 
+   * 
+   * @private
+   * @type {object[]}
+   * @memberof FavouritesPage
+   */
   private favouritesArr: object[];
 
+  /**
+   * Creates an instance of FavouritesPage.
+   * @param {NavController} navCtrl 
+   * @param {ToastController} toastController 
+   * @param {SMS} sms 
+   * @param {FavouriteProvider} favouriteProvider 
+   * @param {Http} http 
+   * 
+   * @memberof FavouritesPage
+   */
   constructor(
     private navCtrl: NavController,
     private toastController: ToastController,
     private sms: SMS,
     private favouriteProvider: FavouriteProvider,
     private http: Http
-  ) {}
-  
-  private _getFavouriteById(id: number):object {
-    for(let i=0; i<this.favouritesArr.length;i++){
-      if(this.favouritesArr[i]['favInfo']['id']===id){
-        return {object: this.favouritesArr[i], index:i};
+  ) { }
+
+  /************* Private variables *****************/
+
+  /**
+   * Gets a favourite by a stop id
+   * 
+   * @private
+   * @param {number} id 
+   * @returns {object} 
+   * 
+   * @memberof FavouritesPage
+   */
+  private _getFavouriteById(id: number): object {
+
+    for (let i = 0; i < this.favouritesArr.length; i++) {
+      if (this.favouritesArr[i]['favInfo']['id'] === id) {
+        return { object: this.favouritesArr[i], index: i };
       }
     }
   }
-  
-  private _getPredictionUrl(route:string, id:number):string{
+
+  /**
+   * Helper function that returns a routes prediction url
+   * 
+   * @private
+   * @param {string} route 
+   * @param {number} id 
+   * @returns {string} 
+   * 
+   * @memberof FavouritesPage
+   */
+  private _getPredictionUrl(route: string, id: number): string {
     return `http://restbus.info/api/agencies/ttc/routes/${route}/stops/${id}/predictions`;
   }
 
 
+  /************* Public variables *****************/
+
+
+  /**
+   * Loads the favourites from storage
+   * 
+   * 
+   * @memberof FavouritesPage
+   */
   public ionViewDidEnter() {
     console.log('loads favourite');
     this.favouritesArr = [];
@@ -53,17 +101,32 @@ export class FavouritesPage {
 
   }
 
+  /**
+   * Remove a favourite from the list
+   * 
+   * @param {number} id 
+   * 
+   * @memberof FavouritesPage
+   */
   public removeFavourite(id: number): void {
     this.favouriteProvider.removeFromFavourites(id).then(value => {
       let object = this._getFavouriteById(id);
-      this.favouritesArr.splice(object['id'], 1)
+
+      this.favouritesArr.splice(object['index'], 1);
     })
       .catch(err => {
         console.log(err);
       });
 
   }
-  
+
+  /**
+   * Gets the live predictions time for each favourite 
+   * 
+   * @param {object[]} favourites 
+   * 
+   * @memberof FavouritesPage
+   */
   public getStopTimes(favourites: object[]): void {
     let favObj: Observable<object>[] = []
 
@@ -95,22 +158,37 @@ export class FavouritesPage {
   }
 
 
+  /**
+   * Sends and SMS to get stop predictions
+   * 
+   * @param {string} code 
+   * 
+   * @memberof FavouritesPage
+   */
   public sendSMS(code: string): void {
     this.sms.send('898882', code)
       .then(value => console.log('sent sms'))
       .catch(err => console.log(err));
   }
-  
-  public getStopPrediction(id: number):void{
+
+  /**
+   * Sets stop predictions on array
+   * element by id
+   * 
+   * @param {number} id 
+   * 
+   * @memberof FavouritesPage
+   */
+  public getStopPrediction(id: number): void {
 
     let favourite = this._getFavouriteById(id);
     let url: string = this._getPredictionUrl(favourite['object']['favInfo']['route'], favourite['object']['favInfo']['id']);
     this.http.get(url).map(res => res.json()).subscribe
-    (
-      data=> this.favouritesArr[favourite['index']]['jsonInfo']=data,
-      err=>console.log(err),
-      ()=>console.log('done updating')
-    );
+      (
+      data => this.favouritesArr[favourite['index']]['jsonInfo'] = data,
+      err => console.log(err),
+      () => console.log('done updating')
+      );
 
   }
 
